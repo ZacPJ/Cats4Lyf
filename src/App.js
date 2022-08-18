7f0470cc6dfd3fbe21753cd99ee5b223e091fc8e
BlazeofTerra <102171681+BlazeofTerra@users.noreply.github.com>
2022-08-18 11:27:05 +0100

    basic eliments

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
 export default function App() {
 	return (
-		<div className="App">
-			
+		<div id="content">
+			<nav>
+				<div id="topBar">
+					<div id="logo">
+						<h1>Cats 4 Life</h1>
+						<img src="" />
+					</div>
+					<img src="" />
+				</div>
+
+				<div id="tagsList">
+					<button>Floofy 15</button>
+					<button>Cute 5</button>
+					<button>Grumpy 1</button>
+				</div>
+			</nav>
+			<div id="content">
+				<div id="postHolder">
+					<div className="catPost">
+						<img />
+					</div>
+				</div>
+			</div>
+			<footer>
+				<p></p>
+				<p></p>
+			</footer>
 		</div>
 	);
 }
